609e9db46b3e0431fe50fa0526ae610129bac52f
Alejandro Burciaga Calzadillas <alejbur@gmail.com>
2024-11-10 16:39:31 -0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'proyectoAPI';
+// }
+
+import { Component } from '@angular/core';
+import { CatSearchComponent } from './components/cat-search/cat-search.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Busqueda de Razas de Gato con Cat API</h1>
+    <app-cat-search></app-cat-search>`,
+  imports: [CatSearchComponent]
+})
+export class AppComponent {}
